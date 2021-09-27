8b2d313d86c75edaed66e6d239c6f71acbbc9dec
Ramon <ramonluciolinhares@gmail.com>
2021-09-27 17:34:39 -0300

    External functions allowed: read, malloc, free. Provided by unistd.h and stdlib.h.

--- get_next_line.h
+++ get_next_line.h
@@ -6,10 +6,14 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 17:43:36 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/09/27 17:22:00 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/09/27 17:32:21 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
-#  define GET_NEXT_LINE_H
-# endif
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
