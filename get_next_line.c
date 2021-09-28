916deb56f5ff73e760cbf742d062bae2c54ece62
Ramon <ramonluciolinhares@gmail.com>
2021-09-28 09:39:38 -0300

    Add 42 comment with get_next_line manual

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,22 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 17:42:23 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/09/28 08:39:12 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/09/28 09:38:26 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/*
+** NAME
+** 		get_next_line
+** DESCRIPTION
+** 		Write a function which returns a line read from a
+** 		file descriptor
+** RETURN VALUE
+** 		Read line: correct behavior
+** 		NULL: nothing else to read or an error occurred
+*/
 
 char	*get_next_line(int fd)
 {
