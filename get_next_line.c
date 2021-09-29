40d8e4e43369b529fc2badc85bbafef99988eef8
Ramon <ramonluciolinhares@gmail.com>
2021-09-29 10:04:40 -0300

    Add get_line function:
        NAME
                        get_line - get a line from buffer
        DESCRIPTION
                        Uses ft_strchr to locate a newline character
                        on buffer
        RETURN VALUE
                        On success, returns a string containing
                        the first line found on buffer
                        If there is no newline character or else,
                        returns NULL

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,55 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 17:42:23 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/09/28 20:25:34 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/09/29 09:59:37 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*getline(char **static_buffer)
-{
+/*
+** NAME
+** 		get_line - get a line from buffer
+** DESCRIPTION
+** 		Uses ft_strchr to locate a newline character
+** 		on buffer
+** RETURN VALUE
+** 		On success, returns a string containing
+** 		the first line found on buffer
+** 		If there is no newline character or else,
+** 		retuns NULL
+*/
 
+static char	*get_line(char **static_buffer)
+{
+	char	*ptr_to_newline_char;
+	size_t	line_size;
+	char	*line;
+	char	*temp_buffer;
+
+	if (*static_buffer == NULL)
+		return (NULL);
+	ptr_to_newline_char = ft_strchr(*static_buffer, '\n');
+	if (ptr_to_newline_char)
+	{
+		line_size = (ptr_to_newline_char - *static_buffer) + 1;
+		line = ft_substr(*static_buffer, 0, line_size);
+		ptr_to_newline_char++;
+		temp_buffer = ft_strdup(ptr_to_newline_char);
+		free(*static_buffer);
+		*static_buffer = ft_strdup(temp_buffer);
+		free(temp_buffer);
+		return (line);
+	}
+	else
+		return (NULL);
 }
 
 /*
 ** NAME
 ** 		get_next_line
 ** DESCRIPTION
-** 		Write a function which returns a line read from a
+** 		A function which returns a line read from a
 ** 		file descriptor
 ** RETURN VALUE
 ** 		Read line: correct behavior
