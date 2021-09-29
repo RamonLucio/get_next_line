7f670eef652cdcd89ca7d4de8384137b2961408d
Ramon <ramonluciolinhares@gmail.com>
2021-09-29 11:30:43 -0300

    Add assign_line function NAME 		assign_line - assign a string to the static variable line DESCRIPTION 		Receive bytes read from a previous call to read on file descriptor, 		and the addresses of the static variables buffer_remainder, buffer 		and line, which were initialized in get_next_line. 		If read was successfully called, join buffer to buffer_remainder, 		call get_line to check if there is a line on the string created. 		If a line was found, assign it to static variable line. 		If read returned EOF, and there is a string stored on buffer_remainder, 		assign it to line and set buffer_remainder to NULL.

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,78 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 17:42:23 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/09/29 10:40:25 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/09/29 11:24:24 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/*
+** NAME
+** 		strlcat -- size-bounded string concatenation
+** DESCRIPTION
+** 		strlcat() appends string src to the end of dst.  It will append at most
+** 		dstsize - strlen(dst) - 1 characters.  It will then NUL-terminate,
+** 		unless dstsize is 0 or the original dst string was longer than dstsize
+** 		(in practice this should not happen as it means that either dstsize is
+** 		incorrect or that dst is not a proper string).
+** 		If the src and dst strings overlap, the behavior is undefined.
+** RETURN VALUES
+** 		strlcat() returns the total length of the string it tried to create.
+** 		That means the initial length of dst plus the length of src.
+*/
+
+static size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	char		*destination;
+	const char	*source;
+	size_t		size;
+	size_t		dst_len;
+
+	destination = dst;
+	source = src;
+	size = dstsize;
+	while (size-- && *destination != '\0')
+		destination++;
+	dst_len = destination - dst;
+	size = dstsize - dst_len;
+	if (size == 0)
+		return (dst_len + ft_strlen(source));
+	while (size-- > 1 && *source != '\0')
+		*destination++ = *source++;
+	*destination = '\0';
+	while (*source++)
+		;
+	return (dst_len + (source - src) - 1);
+}
+
+/*
+** NAME
+** 		ft_strjoin -- concatenate two strings
+** DESCRIPTION
+** 		Allocates (with malloc(3)) and returns a new string,
+** 		which is the result of the concatenation of ’s1’ and ’s2’.
+** RETURN VALUE
+** 		The new string. NULL if the allocation fails.
+*/
+
+static char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	str_size;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str_size = s1_len + s2_len + 1;
+	str = (char *) malloc(str_size * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s1, s1_len + 1);
+	ft_strlcat(str, s2, str_size);
+	return (str);
+}
 
 /*
 ** NAME
@@ -52,6 +119,45 @@ static char	*get_line(char **buffer_remainder)
 
 /*
 ** NAME
+** 		assign_line - assign a string to the static variable line
+** DESCRIPTION
+** 		Receive bytes read from a previous call to read on file descriptor,
+** 		and the addresses of the static variables buffer_remainder, buffer
+** 		and line, which were initialized in get_next_line.
+**
+** 		If read was successfully called, join buffer to buffer_remainder,
+** 		call get_line to check if there is a line on the string created.
+** 		If a line was found, assign it to static variable line.
+**
+** 		If read returned EOF, and there is a string stored on buffer_remainder,
+** 		assign it to line and set buffer_remainder to NULL.
+*/
+
+static void	assign_line(size_t bytes, char **buf_rem, char **buf, char **line)
+{
+	char	*temp;
+
+	if (bytes > 0)
+	{
+		if (*buf_rem == NULL)
+			*buf_rem = ft_strdup("");
+		temp = ft_strjoin(*buf_rem, *buffer);
+		free(*buf_rem);
+		*buf_rem = ft_strdup(temp);
+		free(temp);
+		*line = get_line(buf_rem);
+	}
+	free(*buffer);
+	if (bytes <= 0 && *buf_rem != NULL)
+	{
+		*line = ft_strdup(*buf_rem);
+		free(*buf_rem);
+		*buf_rem = NULL;
+	}
+}
+
+/*
+** NAME
 ** 		get_next_line
 ** DESCRIPTION
 ** 		A function which returns a line read from a
@@ -77,5 +183,7 @@ char	*get_next_line(int fd)
 		if (buffer == NULL)
 			return (NULL);
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		buffer[bytes_read] = '\0';
+		assign_line(bytes_read, &buffer_remainder, &buffer, &line);
 	}
 }
